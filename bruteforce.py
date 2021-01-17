a3a2c22c4f012f0c9c9ac37e41662c804590a7f2
Christopher Gentibano <43651358+cgentibano95@users.noreply.github.com>
2021-01-17 12:32:03 -0800

    Updating bruteforce
    Bruteforce now keeps track of time

--- bruteforce.py
+++ bruteforce.py
@@ -2,8 +2,10 @@ from itertools import product
 from main import MD5Crypt
 import multiprocessing as mp
 import string
+from timeit import default_timer as timer
 
-# Team 41: pass= gzwbuh
+# Team 41: pass = gzwbuh
+# File purely to test password testing throughput
 
 
 def try_onepass(arg_word):
@@ -23,23 +25,35 @@ def try_onepass(arg_word):
     return found_pw
 
 
-def generate_pws():
+def generate_pws(chars_tested):
 
     pw_found = "" #storing pw here
     low_ascii = string.ascii_lowercase
-
+    pw_counter = 0
+    pw_time = 0
     p = mp.Pool(processes=10)
-    for x in p.imap(try_onepass, product(low_ascii, repeat=6), chunksize=10):
+    start = timer()
+    for x in p.imap(try_onepass, (product(low_ascii, repeat=chars_tested)), chunksize=10):
+        pw_counter += 10
         if x != "":
-            p.join()
-            p.close()
-            return x
-    p.join()
+            end = timer()
+            pw_time = pw_counter // (end - start)
+            return x,pw_time
+
+    # if here, we didn't find pass
+    end = timer()
+    pw_time = pw_counter // (end - start)
     p.close()
-    return pw_found
+    p.join()
+    return pw_found,pw_time
 
 
 if __name__ == "__main__":
-
-
-    print("\n\n\n\n\n Found result!!! Done! \n\n\n\n\n\n" + generate_pws())
+    chars_tested = 5
+    print("Trying to find passwords with " + str(chars_tested) + " characters")
+    soln,pw_time = generate_pws(chars_tested)
+    print("Candidates tested per second: " + str(pw_time))
+    if soln == "":
+        print("Didn't find the solution :( ")
+    else:
+        print("The solution is: " + soln)
